54cd220d70f77376de3ab2a1df13ad871979c0a1
karuna24s <karuna24s@gmail.com>
2016-10-08 21:17:31 -0400

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert is_logged_in?
+    #assert is_logged_in?
   end
 end
