f4283762394f2066760b257cb10702f177d4f685
karuna24s <karuna24s@gmail.com>
2016-10-11 11:27:54 -0400

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -86,4 +86,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    karuna = users(:karuna)
+    archer  = users(:archer)
+    assert_not karuna.following?(archer)
+    karuna.follow(archer)
+    assert karuna.following?(archer)
+    assert archer.followers.include?(karuna)
+    karuna.unfollow(archer)
+    assert_not karuna.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    karuna = users(:karuna)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert karuna.feed.include?(post_following)
+    end
+    # Posts from self
+    karuna.microposts.each do |post_self|
+      assert karuna.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not karuna.feed.include?(post_unfollowed)
+    end
+  end
 end
